162ce24736a5d6281818b13d85e4a0b256b80eca
n-zamani <51783521+n-zamani@users.noreply.github.com>
2019-10-05 14:41:50 +0330

    Update other.constants.js

--- src/_constants/other.constants.js
+++ src/_constants/other.constants.js
@@ -1,9 +1,9 @@
-export const HOST = 'https://woocommerce.niloofar-zamani.ir/wp-json/wc/v3/';
+export const HOST = 'https://YOUR_WEBSITE/wp-json/wc/v3/';
 
-export const KEY = 'consumer_key=ck_68bcd1bde746fa223efc798125f14b21563f69fb&consumer_secret=cs_d913cba711759245653aa93a41ae7b8076c330e2';
+export const KEY = 'consumer_key=YOUR_CONSUMER_KEY&consumer_secret=YOUR_CONSUMER_SECRET';
 
-export const TIMEOUT_DELAY = 10000;
+// export const TIMEOUT_DELAY = 10000;
 
-export const realtimeDate = {
-    userData: null
-};
+// export const realtimeDate = {
+//     userData: null
+// };
